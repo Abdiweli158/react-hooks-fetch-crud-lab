da05003b638e1c70f4e726fa94f216898fc5c8e0
Abdiweli158 <walicade158@gmail.com>
2024-02-15 00:26:24 +0300

    completed asignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({ onAddQuestion }) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
+
     answer2: "",
     answer3: "",
     answer4: "",
@@ -19,9 +20,43 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    
+    const formPost = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: parseInt(formData.correctIndex),
+    };
+
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formPost),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        onAddQuestion(data);
+        setFormData({
+          prompt: "",
+          answer1: "",
+          answer2: "",
+          answer3: "",
+          answer4: "",
+          correctIndex: 0,
+        });
+      })
+      .catch((error) => {
+        console.error("Error adding question:", error);
+      });
   }
 
+  }
   return (
     <section>
       <h1>New Question</h1>
@@ -88,6 +123,6 @@ function QuestionForm(props) {
       </form>
     </section>
   );
-}
+
 
 export default QuestionForm;
